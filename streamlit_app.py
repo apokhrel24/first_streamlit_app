0b93595d48b029fa3d6176af6105bfae37435d0e
apokhrel24 <arun.pokhrel24@gmail.com>
2023-09-20 17:22:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega' 3 & Blueberyy Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.test('Hard-Boiled Free-Range Egg')
+
+
+
